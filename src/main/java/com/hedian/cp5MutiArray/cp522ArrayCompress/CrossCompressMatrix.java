b614903e54e7086d42d56353d3b8a8a3b0bdf1af
gjyang <1457659650@qq.com>
2020-07-06 18:01:17 +0800

    init15

--- src/main/java/com/hedian/cp5MutiArray/cp522ArrayCompress/CrossCompressMatrix.java
+++ src/main/java/com/hedian/cp5MutiArray/cp522ArrayCompress/CrossCompressMatrix.java
@@ -3,6 +3,8 @@ package com.hedian.cp5MutiArray.cp522ArrayCompress;
 import com.hedian.cp5MutiArray.cp51MutiArray.Matrix;
 import com.hedian.cp5MutiArray.cp51MutiArray.MyMatrix;
 
+import java.util.Currency;
+
 public class CrossCompressMatrix implements MyMatrix {
 
     private int rows;
@@ -35,7 +37,7 @@ public class CrossCompressMatrix implements MyMatrix {
     public int get(int i, int j) {
         return 0;
     }
-
+    //
     @Override
     public void set(int i, int j, int value) {
         if (value == 0) {
@@ -47,18 +49,100 @@ public class CrossCompressMatrix implements MyMatrix {
         Triple newTriple = new Triple(i, j, value);
         CrossNode newCrossNode = new CrossNode(newTriple, null, null);
         //行
-        if (this.rowheads == null) {
-            //行头插入
-            this.rowheads[i] = new CrossNode(newTriple, this.rowheads[i], null);
+        if (this.rowheads[i] == null || this.rowheads[i].triple.col > j) {
+            //行头插入或空行插入
+            newCrossNode.right = this.rowheads[i];
+            this.rowheads[i] = newCrossNode;
         } else {
-            //非头插入
+            //非头插入非空行
+            CrossNode frontNode = null;
             CrossNode currentNode = this.rowheads[0];
             while (currentNode != null) {
-                if (currentNode.triple.compareTo(newTriple) > 0) {
-
+                //> 下一节点
+                //== set
+                //< 新增节点
+                if (newCrossNode.triple.col < currentNode.triple.col) {
+                    frontNode.right = newCrossNode;
+                    newCrossNode.right = currentNode;
+                    break;
+                } else if (newCrossNode.triple.col == currentNode.triple.col) {
+                    CrossNode rightNode = currentNode.right;
+                    newCrossNode.right = rightNode;
+                    frontNode.right = newCrossNode;
+                    break;
                 }
+                frontNode = currentNode;
                 currentNode = currentNode.right;
             }
+            frontNode.right = new CrossNode(newTriple, null, null);
+        }
+        //列
+        CrossNode colhead = this.colheads[j];
+        if (colhead == null || colhead.triple.row > i) {
+            //头插入
+            newCrossNode.down = colhead;
+            colhead = newCrossNode;
+        } else {
+            //非头插入
+            CrossNode frontNode = null;
+            CrossNode currentNode = colhead;
+            while (currentNode != null && currentNode.triple.row <= newCrossNode.triple.row) {
+                frontNode = currentNode;
+                currentNode = currentNode.down;
+            }
+            frontNode.down = newCrossNode;
+            newCrossNode.down = currentNode;
         }
     }
+
+    @Override
+    public String toString()                               //返回稀疏矩阵三元组十字链表和稀疏矩阵字符串
+    {
+        String str="三元组十字链表\n";
+        str+="三元组行的单链表：";
+        for (int i=0; i<this.rowheads.length; i++)
+        {
+            str+="(";
+            for (CrossNode p=this.rowheads[i];  p!=null;  p=p.right)
+            {
+                str += p.triple.toString();
+                if (p.right!=null)
+                    str+=",";
+            }
+            str += ")";
+            if (i<this.rowheads.length-1)
+                str += ", ";
+        }
+        str+="\n三元组列的单链表：";
+        for (int i=0; i<this.colheads.length; i++)
+        {
+            str+="(";
+            for (CrossNode p=this.colheads[i];  p!=null;  p=p.down)
+            {
+                str += p.triple.toString();
+                if (p.down!=null)
+                    str+=",";
+            }
+            str += ")";
+            if (i<this.colheads.length-1)
+                str += ", ";
+        }
+
+        str+="\n稀疏矩阵"+this.getClass().getName()+"（"+rows+"×"+cols+"）：\n";
+        for (int i=0; i<this.rows; i++)
+        {
+            CrossNode p=this.rowheads[i];
+            for (int j=0; j<this.cols; j++)
+                if (p!=null && j==p.triple.col)            //有i==p.data.row
+                {
+                    str += String.format("%4d",p.triple.number);
+                    p = p.right;
+                }
+                else
+                    str +=String.format("%4d",0);
+            str += "\n";
+        }
+        return str;
+    }
+
 }
